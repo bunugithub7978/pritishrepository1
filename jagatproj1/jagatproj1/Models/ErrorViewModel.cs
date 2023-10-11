bac0b2f8bbc1a8e2a0b888b0710eea1a72af5d64
nayakpritish79782 <nayakpritish79782@gmail.com>
2023-10-11 10:53:49 +0530

    my github project

---
+++ jagatproj1/jagatproj1/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace jagatproj1.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
